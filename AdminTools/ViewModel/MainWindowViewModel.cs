19836ea58ab10b9b2a403cd5990ddaaa2b2b6fda
Amidomaru <igor.zharikov@gmail.com>
2015-06-04 23:51:24 +0300

    Work with Scanner

--- AdminTools/ViewModel/MainWindowViewModel.cs
+++ AdminTools/ViewModel/MainWindowViewModel.cs
@@ -10,17 +10,21 @@ namespace AdminTools.ViewModel
     {
         public RelayCommand UsersEditorCommand { get; private set; }
         public RelayCommand PermissionsTreeCommand { get; private set; }
+        public RelayCommand ScanDocumentsCommand { get; private set; }
 
         private UserEditorViewModel userEditorViewModel;
         private PermissionsTreeViewModel permissionsTreeViewModel;
+        private ScanDocumentsViewModel scanDocumentsViewModel;
 
-        public MainWindowViewModel(UserEditorViewModel userEditorViewModel, PermissionsTreeViewModel permissionsTreeViewModel)
+        public MainWindowViewModel(UserEditorViewModel userEditorViewModel, PermissionsTreeViewModel permissionsTreeViewModel, ScanDocumentsViewModel scanDocumentsViewModel)
         {
             this.userEditorViewModel = userEditorViewModel;
             this.permissionsTreeViewModel = permissionsTreeViewModel;
+            this.scanDocumentsViewModel = scanDocumentsViewModel;
             
             this.UsersEditorCommand = new RelayCommand(this.UsersEditor);
-            this.PermissionsTreeCommand = new RelayCommand(this.PermissionsTree);      
+            this.PermissionsTreeCommand = new RelayCommand(this.PermissionsTree);
+            this.ScanDocumentsCommand = new RelayCommand(this.ScanDocuments);   
         }
 
         public void UsersEditor()
@@ -31,6 +35,11 @@ namespace AdminTools.ViewModel
         public void PermissionsTree()
         {
             (new PermissionsTreeView() { DataContext = permissionsTreeViewModel }).ShowDialog();
+        }
+
+        public void ScanDocuments()
+        {
+            (new ScanDocumentsView() { DataContext = scanDocumentsViewModel }).ShowDialog();
         }
     }
 }
---
+++ Services/Interface/IScannerService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLib;
+
+namespace Core
+{
+    public interface IScannerService
+    {
+        
+    }
+}
