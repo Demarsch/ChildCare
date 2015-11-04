8a3ec68abc8621a86a254c483c8a0e0b84da573b
Andrey Chaplygin <demarsch@yandex.ru>
2015-11-04 18:06:32 +0100

    Closes #88 - Unit tests
    Closed #103 - Unit tests

---
+++ Core.Test/Extensions/CacheServiceExtensionsTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Core.Extensions;
+using Core.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace Core.Test.Extensions
+{
+    [TestFixture]
+    public class CacheServiceExtensionsTest
+    {
+        [Test]
+        public void AutoWire_ThrowsExceptionWhenThereIsNoIdProperty()
+        {
+            var cacheService = new Mock<ICacheService>().Object;
+            var invalidClass = new InvalidClassToAutoWire();
+            Assert.Throws<InvalidOperationException>(() => cacheService.AutoWire(invalidClass, x => x.CachedObject));
+        }
+
+        [Test]
+        public void AutoWire_SetsNullWhenCacheHasNoObject()
+        {
+            var mock = new Mock<ICacheService>();
+            mock.Setup(x => x.GetItemById<CachedObject>(It.IsAny<int>())).Returns((CachedObject)null);
+            var cacheService = mock.Object;
+            var classToAutowire = new ValidClassToAutoWire { CachedObject = new CachedObject() };
+            cacheService.AutoWire(classToAutowire, x => x.CachedObject);
+            Assert.IsNull(classToAutowire.CachedObject);
+        }
+
+        [Test]
+        public void AutoWire_SetsObjectFromCache()
+        {
+            var mock = new Mock<ICacheService>();
+            var cachedObject = new CachedObject { Id = 1, Name = "One" };
+            mock.Setup(x => x.GetItemById<CachedObject>(1)).Returns(cachedObject);
+            var cacheService = mock.Object;
+            var classToAutowire = new ValidClassToAutoWire { CachedObjectId = 1 };
+            cacheService.AutoWire(classToAutowire, x => x.CachedObject);
+            Assert.AreSame(cachedObject, classToAutowire.CachedObject);
+        }
+
+        private class InvalidClassToAutoWire
+        {
+            public CachedObject CachedObject { get; set; }
+        }
+
+        private class ValidClassToAutoWire
+        {
+            public int CachedObjectId { get; set; }
+
+            public CachedObject CachedObject { get; set; }
+        }
+
+        private class CachedObject
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ Core.Test/Misc/ChangeTrackerTest.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using Core.Misc;
+using NUnit.Framework;
+
+namespace Core.Test.Misc
+{
+    [TestFixture]
+    public class ChangeTrackerTest
+    {
+        private int X { get; set; }
+
+        [Test]
+        public void Track_DoesNothingWhenTrackerIsDisabled()
+        {
+            var target = new ChangeTracker();
+            target.Track(0, 1, "X");
+            Assert.IsFalse(target.HasChanges);
+        }
+
+        [Test]
+        public void Track_TracksChangesWhenEnabled()
+        {
+            var target = new ChangeTracker { IsEnabled = true };
+            target.Track(0, 1, "X");
+            Assert.IsTrue(target.HasChanges);
+            Assert.IsTrue(target.PropertyHasChanges(() => X));
+        }
+
+        [Test]
+        public void Track_UsesRegisteredComparer()
+        {
+            var target = new ChangeTracker { IsEnabled = true };
+            var comparer = new CustomComparer();
+            target.RegisterComparer(() => X, comparer);
+            target.Track(0, 1, "X");
+            target.Track(1, 2, "X");
+            Assert.IsTrue(comparer.IsEqualsInvoked);
+        }
+
+        [Test]
+        public void Track_UntracksChangesWhenOriginalValueIsSet()
+        {
+            var target = new ChangeTracker { IsEnabled = true };
+            target.Track(0, 1, "X");
+            target.Track(1, 0, "X");
+            Assert.IsFalse(target.HasChanges);
+            Assert.IsFalse(target.PropertyHasChanges(() => X));
+        }
+
+        [Test]
+        public void Untrack_DoesNothingIfPropertyHasNoChanges()
+        {
+            var target = new ChangeTracker { IsEnabled = true };
+            int value = -1;
+            target.Track(0, 1, "y");
+            target.Untrack(ref value, () => X);
+            Assert.AreEqual(-1, value);
+        }
+
+        [Test]
+        public void Untrack_SetsOriginalValueIfPropertyHasChanges()
+        {
+            var target = new ChangeTracker { IsEnabled = true };
+            int originalValueSource = 0;
+            target.Track(originalValueSource, 1, "X");
+            originalValueSource = 1;
+            target.Untrack(ref originalValueSource, () => X);
+            Assert.AreEqual(0, originalValueSource);
+            Assert.IsFalse(target.HasChanges);
+            Assert.IsFalse(target.PropertyHasChanges(() => X));
+        }
+
+        [Test]
+        public void IsEnabled_DisablingClearAllChanges()
+        {
+            var target = new ChangeTracker { IsEnabled = true };
+            target.Track(0, 1, "X");
+            target.IsEnabled = false;
+            Assert.IsFalse(target.HasChanges);
+        }
+
+        private class CustomComparer : IEqualityComparer<int>
+        {
+            public bool IsEqualsInvoked { get; private set; }
+
+            public bool Equals(int x, int y)
+            {
+                IsEqualsInvoked = true;
+                return x == y;
+            }
+
+            public int GetHashCode(int obj)
+            {
+                return obj.GetHashCode();
+            }
+        }
+    }
+}
