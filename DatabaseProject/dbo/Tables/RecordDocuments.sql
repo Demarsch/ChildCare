606bc1aea3f5457160fd4861bb6d6f7420534820
Amidomaru <igor.zharikov@gmail.com>
2015-11-17 21:15:41 +0300

    Schema changes: added RecordDocuments

---
+++ DatabaseProject/dbo/Tables/RecordDocuments.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[RecordDocuments] (
+    [Id]         INT IDENTITY (1, 1) NOT NULL,
+    [RecordId]   INT NOT NULL,
+    [DocumentId] INT NOT NULL,
+    CONSTRAINT [PK_RecordDocuments] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_RecordDocuments_Documents] FOREIGN KEY ([DocumentId]) REFERENCES [dbo].[Documents] ([Id]),
+    CONSTRAINT [FK_RecordDocuments_Records] FOREIGN KEY ([RecordId]) REFERENCES [dbo].[Records] ([Id])
+);
+
