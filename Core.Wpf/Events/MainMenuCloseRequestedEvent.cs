e5960308205ea2917c19d531386f73db3ce05e38
Andrei Chaplygin <andrei.chaplygin@ubs.com>
2015-12-17 14:51:39 +0100

    Added missed files

---
+++ Core.Wpf/Events/MainMenuCloseRequestedEvent.cs
@@ -0,0 +1,8 @@
+﻿using Prism.Events;
+
+namespace Core.Wpf.Events
+{
+    public class MainMenuCloseRequestedEvent : PubSubEvent<object>
+    {
+    }
+}
---
+++ Core.Wpf/Events/SelectionChangedEvent.cs
@@ -0,0 +1,8 @@
+﻿using Prism.Events;
+
+namespace Core.Wpf.Events
+{
+    public class SelectionChangedEvent<T> : PubSubEvent<int>
+    {
+    }
+}
