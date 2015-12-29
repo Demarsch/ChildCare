c96ac52e8180be267d33201ec82b9098cb3551ab
Andrei Chaplygin <andrei.chaplygin@ubs.com>
2015-12-29 16:26:50 +0100

    #143 - added context menu to Ribbon. Further work is blocked by visual bugs in Ribbon

---
+++ Shell/RibbonEx.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Fluent;
+using ContextMenu = System.Windows.Controls.ContextMenu;
+
+namespace Shell
+{
+    public class RibbonEx : Ribbon
+    {
+        public static readonly DependencyProperty ContextMenuOverrideProperty = DependencyProperty.Register("ContextMenuOverride", typeof (ContextMenu), typeof (RibbonEx), new PropertyMetadata(OnContextMenuChanged));
+
+        private static void OnContextMenuChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.OldValue != null)
+            {
+                ((ContextMenu)e.OldValue).PlacementTarget = null;
+            }
+            if (e.NewValue != null)
+            {
+                ((ContextMenu)e.NewValue).PlacementTarget = (UIElement)d;
+            }
+        }
+
+        public ContextMenu ContextMenuOverride
+        {
+            get { return (ContextMenu)GetValue(ContextMenuOverrideProperty); }
+            set { SetValue(ContextMenuOverrideProperty, value); }
+        }
+
+        protected override void OnContextMenuOpening(ContextMenuEventArgs e)
+        {
+            if (e.Source is IQuickAccessItemProvider)
+            {
+                base.OnContextMenuOpening(e);
+                return;
+            }
+            var contextMenu = ContextMenuOverride;
+            if (contextMenu != null)
+            {
+                contextMenu.IsOpen = true;
+            }
+            e.Handled = true;
+        }
+    }
+}
--- Shell/ShellWindow.xaml.cs
+++ Shell/ShellWindow.xaml.cs
@@ -1,5 +1,15 @@
-﻿using Core.Wpf.Views;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows;
+using Core.Wpf.Views;
+using Fluent;
 using Microsoft.Practices.Unity;
+using Prism.Regions;
 
 namespace Shell
 {
@@ -8,9 +18,53 @@ namespace Shell
     /// </summary>
     public partial class ShellWindow
     {
+        private readonly Dictionary<string, bool> ribbonTabVisibility = new Dictionary<string, bool>(); 
+        
         public ShellWindow()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+            ribbon.Tabs.CollectionChanged += RibbonTabsOnCollectionChanged;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Loaded -= OnLoaded;
+            try
+            {
+                var isolatedStorage = IsolatedStorageFile.GetUserStoreForAssembly();
+                using (var reader = new StreamReader(new IsolatedStorageFileStream(typeof(ShellWindow).FullName, FileMode.OpenOrCreate, isolatedStorage)))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        if (string.IsNullOrEmpty(line))
+                        {
+                            continue;
+                        }
+                        var words = line.Split(' ');
+                        ribbonTabVisibility.Add(words[0], bool.Parse(words[1]));
+                    }
+                }
+            }
+            catch
+            {
+                //Do nothing as this is an optional step
+            }
+        }
+
+        private void RibbonTabsOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems == null)
+            {
+                return;
+            }
+            foreach (var newTab in e.NewItems.Cast<RibbonTabItem>())
+            {
+                bool value;
+                var isVisible = !ribbonTabVisibility.TryGetValue(GetSettingName(newTab), out value) || value;
+                newTab.Visibility = isVisible == false ? Visibility.Collapsed : Visibility.Visible;
+            }
         }
 
         [Dependency]
@@ -18,6 +72,36 @@ namespace Shell
         {
             get { return DataContext as ShellWindowViewModel; }
             set { DataContext = value; }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            ribbon.Tabs.CollectionChanged -= RibbonTabsOnCollectionChanged;
+            foreach (var ribbonTab in ribbon.Tabs)
+            {
+                ribbonTabVisibility[GetSettingName(ribbonTab)] = ribbonTab.Visibility == Visibility.Visible;
+            }
+            try
+            {
+                var isolatedStorage = IsolatedStorageFile.GetUserStoreForAssembly();
+                using (var writer = new StreamWriter(new IsolatedStorageFileStream(typeof (ShellWindow).FullName, FileMode.Create, isolatedStorage)))
+                {
+                    foreach (var item in ribbonTabVisibility)
+                    {
+                        writer.WriteLine("{0} {1}", item.Key, item.Value);
+                    }
+                }
+            }
+            catch
+            {
+                //Do nothing as this is an optional step
+            }
+            base.OnClosed(e);
+        }
+
+        private string GetSettingName(RibbonTabItem ribbonTab)
+        {
+            return string.IsNullOrEmpty(ribbonTab.Name) ? ribbonTab.GetType().FullName : ribbonTab.Name;
         }
     }
 }
