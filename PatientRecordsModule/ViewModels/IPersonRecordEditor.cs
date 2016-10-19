2d1e923ec1ff9327e2a404cac89b4d12aeabb4e3
llmaxxxll <max_luc@mail.ru>
2016-10-19 15:44:20 +0400

    Fixed error with editing records

--- PatientRecordsModule/ViewModels/IPersonRecordEditor.cs
+++ PatientRecordsModule/ViewModels/IPersonRecordEditor.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Shared.PatientRecords.ViewModels
 {
-    public interface IPersonRecordEditor
+    public interface IPersonRecordEditor : INotifyPropertyChanged
     {
+        bool IsViewModeInCurrentProtocolEditor { get; }
+        bool IsEditModeInCurrentProtocolEditor { get; }
+        bool IsRecordCanBeCompleted { get; set; }
+        bool AllowDocuments { get; set; }
+        bool AllowDICOM { get; set; }
+        bool CanAttachDICOM { get; set; }
+        bool CanDetachDICOM { get; set; }
+
         void SetRVAIds(int visitId, int assignmentId, int recordId);
+
+        ICommand PrintProtocolCommand { get; }
+        ICommand SaveProtocolCommand { get; }
+        ICommand ShowInEditModeCommand { get; }
+        ICommand ShowInViewModeCommand { get; }
+        ICommand AttachDocumentCommand { get; }
+        ICommand DetachDocumentCommand { get; }
+        ICommand AttachDICOMCommand { get; }
+        ICommand DetachDICOMCommand { get; }
     }
 }
